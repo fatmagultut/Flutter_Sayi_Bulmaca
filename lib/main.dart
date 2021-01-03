e616b4deecb72c9560ebd59048b94f0d4e5f7788
Rosa <34123120+fatmagultut@users.noreply.github.com>
2021-01-03 18:51:38 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       //Hazır MaterialApp widgetını kullanıyorum.
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: new ThemeData(
         primarySwatch: Colors.purple,
@@ -25,42 +27,77 @@ class MyCrossIntt extends StatefulWidget {
 }
 
 class _MyCrossInttState extends State<MyCrossIntt> {
-  int counter = 0; //0 dan 25 e count değerini tutacak
+  static const duration = const Duration(seconds: 1);
+  int counter = 1; //1 dan 25 e count değerini tutacak
+  List<int> valList = myList();
 
-  void incrementCounter() {
+  int secondsPassed = 0;
+  bool isActive = false;
+
+  Timer timer;
+
+  void handleTick() {
     setState(() {
-      //setState() StatefulWidgetlarda ekranın yeniden çizilmesini sağlıyor.
-      counter++;
+      secondsPassed = secondsPassed + 1;
     });
+  }
+
+  static List<int> myList() {
+    List<int> numbers = List.generate(25, (index) => index + 1);
+    numbers.shuffle();
+    return numbers;
   }
 
   @override
   Widget build(BuildContext context) {
+    if (timer == null) {
+      timer = Timer.periodic(duration, (Timer t) {
+        handleTick();
+      });
+    }
+    int seconds = secondsPassed % 60;
+    int minutes = secondsPassed ~/ 60;
+    int hours = secondsPassed ~/ (60 * 60);
+    String val;
     return new Scaffold(
       appBar: new AppBar(
         title: Center(child: new Text('Sayı Bulmaca')),
       ),
-      body: new GridView.count(
-        crossAxisCount: 5,
-        children: new List<Widget>.generate(25, (index) {
-          return new GridTile(
-            child: new InkWell(
-              //Tıklandığında efekt verip renk değiştiriyor.
+      body: Container(
+        child: GridView.count(
+          crossAxisCount: 5,
+          children: new List<Widget>.generate(25, (index) {
+            val = valList[index].toString();
+            return new GridTile(
+              child: new InkWell(
+                //Tıklandığında efekt verip renk değiştiriyor.
 
-              splashColor: Colors.purple,
-              onTap: () {
-                print('$index');
-              },
-              child: new Card(
-                color: Colors.grey[300],
-                child: new Center(
-                  child: new Text('$index'),
+                splashColor: Colors.purple,
+                onTap: () => onClickValues(index),
+                child: new Card(
+                  color: Colors.grey[300],
+                  child: new Center(
+                    child: new Text('$val'),
+                  ),
                 ),
               ),
-            ),
-          );
-        }),
+            );
+          }),
+        ),
       ),
     );
+  }
+
+  onClickValues(int index) {
+    if (valList[index] == counter) {
+      setState(() {
+        counter++;
+      });
+      print(valList[index].toString());
+      if (valList[index] == 25) {
+        counter = 1;
+        print("oyun bitti");
+      }
+    }
   }
 }
